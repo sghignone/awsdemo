da61d94e23a4c0e3e060899f3884ca697807a161
Stefano Ghignone <sghignone@users.noreply.github.com>
2019-03-28 15:19:04 +0100

    Update demoscript.sh
    code updated

--- demoscript.sh
+++ demoscript.sh
@@ -3,4 +3,4 @@ mkdir DATA
 cd DATA
 wget -c ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
 gunzip -c uniprot_sprot.fasta.gz  | grep -c ">" 
-echo "done"
+echo "`date`: done"
