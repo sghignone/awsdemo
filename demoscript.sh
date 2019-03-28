62216c31e7c7eaef069a6b3505c51e8a7f80a2f3
Stefano Ghignone <sghignone@users.noreply.github.com>
2019-03-28 15:06:13 +0100

    Create demoscript.sh

---
+++ demoscript.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+mkdir DATA
+cd DATA
+wget -c ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+gunzip -c uniprot_sprot.fasta.gz  | grep -c ">" 
+echo "done"
